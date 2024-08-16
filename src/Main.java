2b30b02e80aa506b7a2665522b570c0e1ae2b5d9
nabwae <richard.sevc@gmail.com>
2024-08-16 20:42:48 +0200

    scratchMult draft

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
             System.out.println("Two numbers are required. One for number representing A and second one representing B.");
         } else {
             System.out.println(multBig(args[0], args[1]));
-            System.out.println(scratchMult(args[0], args[1]));
+        //    System.out.println(scratchMult(args[0], args[1]));
         }
     }
 }
