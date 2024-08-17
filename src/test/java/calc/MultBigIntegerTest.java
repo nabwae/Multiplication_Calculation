b1516ec7e2ae6e87cd8b5037f9d0c2a2d24179dc
nabwae <richard.sevc@gmail.com>
2024-08-17 18:53:32 +0200

    More tests added, code clean-up

--- src/test/java/calc/MultBigIntegerTest.java
+++ src/test/java/calc/MultBigIntegerTest.java
@@ -7,14 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MultBigIntegerTest {
     @Test
     void testValuesFromAssignment(){
-        var bigInteger = new MultBigInteger();
-        String multBigOutput = String.valueOf(bigInteger.multBig("12345678901234567890", "11111111111111111111"));
+        String multBigOutput = String.valueOf(MultBigInteger.multBig("12345678901234567890", "11111111111111111111"));
         assertEquals( "137174210013717420998628257899862825790", multBigOutput);
     }
     @Test
     void testNumberTwo(){
-        var bigInteger = new MultBigInteger();
-        String multBigOutput = String.valueOf(bigInteger.multBig("789654", "21456"));
+        String multBigOutput = String.valueOf(MultBigInteger.multBig("789654", "21456"));
         assertEquals( "16942816224", multBigOutput);
+    }
+    @Test
+    void testNumberThree(){
+        String multBigOutput = String.valueOf(MultBigInteger.multBig("456", "89"));
+        assertEquals( "40584", multBigOutput);
     }
 }
--- src/test/java/calc/MultFromScratchTest.java
+++ src/test/java/calc/MultFromScratchTest.java
@@ -7,12 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MultFromScratchTest {
     @Test
     void testValuesFromAssignment(){
-        var MultFromScratch = new MultFromScratch();
         assertEquals( "137174210013717420998628257899862825790", MultFromScratch.scratchMult("12345678901234567890", "11111111111111111111"));
     }
     @Test
     void testNumberTwo(){
-        var MultFromScratch = new MultFromScratch();
         assertEquals( "16942816224", MultFromScratch.scratchMult("789654", "21456"));
+    }
+    @Test
+    void testNumberThree(){
+        assertEquals( "40584", MultFromScratch.scratchMult("456", "89"));
     }
 }
