9a669420f6973e2d48a9434b8d33ef01bc60db83
nabwae <richard.sevc@gmail.com>
2024-08-17 14:45:37 +0200

    Algorithm choice added

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,28 @@ import static calc.MultFromScratch.*;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length != 2) {
-            System.out.println("Two numbers are required. One for number representing A and second one representing B.");
+        if(args.length != 3) {
+            // If user did not specify 3 arguments, print warning and usage info
+            System.out.println("Invalid number of arguments used. Usage: --alg1/--alg2 <num1> <num2>");
         } else {
-            System.out.println(multBig(args[0], args[1]));
-            System.out.println(scratchMult(args[0], args[1]));
+            // Load args
+            String algorithm = args[0];
+            String num1 = args[1];
+            String num2 = args[2];
+
+            switch(algorithm){
+                case "--alg1": // Call multiplication using BigInteger
+                    System.out.println(multBig(num1, num2));
+                    break;
+                case "--alg2": // Call multiplication using implementation from scratch
+                    System.out.println(scratchMult(num1, num2));
+                    break;
+                default: // Warn user if algorithm (args[0]) does not match --alg1 or --alg2
+                    System.out.println("Invalid algorithm choice. Usage: --alg1 or --alg2");
+                    break;
+            }
+//            System.out.println(multBig(args[0], args[1]));
+//            System.out.println(scratchMult(args[0], args[1]));
         }
     }
 }
