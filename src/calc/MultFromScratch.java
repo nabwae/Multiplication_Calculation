f75ea4585449db9ff26eaf760d686de0722c2213
nabwae <richard.sevc@gmail.com>
2024-08-17 11:36:28 +0200

    scratchMult progress

--- src/calc/MultFromScratch.java
+++ src/calc/MultFromScratch.java
@@ -8,10 +8,21 @@ public class MultFromScratch {
         int len2 = num2.length();
 
         // prolly have to store result in an array and somewhat split because it wont even fit into long
+        int[] result = new int[len1 + len2];
+
+        for(int i = len1 - 1; i >= 0; i--){
+            int digit2 = Integer.parseInt(String.valueOf(num2.charAt(i)));
+            for(int j = len2 - 1; j >= 0; j--){
+                int digit1 = Integer.parseInt(String.valueOf(num1.charAt(j)));
+
+            }
+        }
 
 
 
-        // draft in head, will perform digit bz digit multiplication with carry, yet to implement
+
+
+        // draft in head, will perform digit by digit multiplication with carry, yet to implement
         return 0;
     }
 }
